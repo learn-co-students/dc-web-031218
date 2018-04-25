b1e4ca13a8522f907e1d8ed6857b36f2108ac477
Rob Cobb <rwcobbjr@gmail.com>
2018-04-25 17:57:21 -0400

    functional library review

--- 30-functional-library-review/index.js
+++ 30-functional-library-review/index.js
@@ -4,57 +4,75 @@ fi = (function() {
       return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0";
     },
 
-    each: function(collection, func) {
+    each: function(collection, callback) {
+      // iterate through the collection, call the callback for each element
+      // collection can be _either_ an array or a POJO
+      // { a: 'b' }
       for (let key in collection) {
-        func(collection[key]);
+        callback(collection[key]);
       }
+      // for an array, x will be the values in the array
+      // for an object, x won't be anything
+      // for (let x of collection) {
+      //   console.log(x);
+      // }
       return collection;
     },
 
-    map: function(collection, func) {
-      let mapped = [];
+    // iterates through a collection
+    // calls the function at each step
+    // returns a new array with the return values from calling the function
+    map: function(collection, callback) {
+      let newArray = [];
       for (let key in collection) {
-        mapped.push(func(collection[key]));
+        newArray.push(callback(collection[key], key, collection));
       }
-      return mapped;
+      return newArray;
     },
 
-    reduce: function(collection, func, accumulator = 0) {
+    // fold
+    // call the callback with the value at that step, and an accumulator, which is the return value from the previous step
+    // either start with some start value
+    // or do something without a start
+    reduce: function(collection, callback, accumulator = 0) {
+      console.log("starting accumulator", accumulator);
       for (let key in collection) {
-        accumulator = func(accumulator, collection[key], collection);
+        accumulator = callback(accumulator, collection[key], collection);
       }
       return accumulator;
     },
 
-    find: function(collection, func) {
-      for (let key in collection) {
-        if (func(collection[key])) {
-          return true;
+    // take an array, possibly be nested, returns array of only one level
+    flatten: function(array, single) {
+      // how should we solve this?
+      // simple = ["a", ["b"]];
+      // => ["a", "b"]
+      let results = [];
+      for (let key in array) {
+        let element = array[key];
+        if (Array.isArray(element)) {
+          // if the element is an array, add its elements to our results after flattening
+          let flattened = element;
+          if (!single) {
+            flattened = fi.flatten(element);
+          }
+          for (let i in flattened) {
+            results.push(flattened[i]);
+          }
+        } else {
+          // if the element is just an element, add it to our results
+          results.push(array[key]);
         }
       }
-      return false;
-    },
-
-    filter: function(collection, func) {
-      let filtered = [];
-      for (let key in collection) {
-        if (func(collection[key])) {
-          filtered.push(collection[key]);
-        }
-      }
-      return filtered;
-    },
-
-    size: function(collection) {
-      let count = 0;
-      for (let key in collection) {
-        count++;
-      }
-      return count;
-    },
-
-    first: function(collection, n = 1) {}
+      console.log("flattened");
+      console.log(array);
+      console.log(results);
+      return results;
+    }
   };
 })();
+
+// flatten
+// uniq
 
 fi.libraryMethod();

96405f5b95d14c73cc56f3cd0c8bffdd71ef1863
Rob Cobb <rwcobbjr@gmail.com>
2018-05-21 18:02:05 -0400

    minor cleanup

--- 51-react-router/youtubr/src/YouTubeContainer.js
+++ 51-react-router/youtubr/src/YouTubeContainer.js
@@ -12,14 +12,13 @@ class YouTubeContainer extends Component {
     super();
 
     this.state = {
-      term: "dolphin",
       videos: [],
       selectedVideo: null
     };
   }
 
   componentDidMount() {
-    this.videoSearch(this.state.term);
+    this.videoSearch();
   }
 
   videoSearch(term) {
@@ -59,37 +58,5 @@ class YouTubeContainer extends Component {
     );
   }
 }
-
-// const response = [
-//   {
-//     etag: '"m2yskBQFythfE4irbTIeOgYYfBU/XoCknp0EO0YWZcuTVD_GhkJp_-M"',
-//     id: { kind: 'youtube#video', videoId: 'KMZ7oOCXfP8' },
-//     snippet: {
-//       title:
-//         'Super Smart Dolphin Answers Questions - Extraordinary Animals - Earth',
-//       description:
-//         'The Echo Location Visualization and Interface System (ELVIS) allows Dolphins to make choices and answer questions. Luna the young Dolphin grasped the ...',
-//       thumbnails: {
-//         default: {
-//           url: 'https://i.ytimg.com/vi/KMZ7oOCXfP8/default.jpg'
-//         }
-//       }
-//     }
-//   },
-//   {
-//     etag: '"m2yskBQFythfE4irbTIeOgYYfBU/z_l0ThWRPFnv9um1eRM_O1LVUqU"',
-//     id: { kind: 'youtube#video', videoId: '3Bk6VPWGi1o' },
-//     snippet: {
-//       title: 'The Complete 2016 SeaWorld "Blue Horizons" Dolphin Show',
-//       description:
-//         'Please SUBSCRIBE by clicking here: http://www.youtube.com/subscription_center?add_user=MoneySavingVideos To see my entire SeaWorld playlist click ...',
-//       thumbnails: {
-//         default: {
-//           url: 'https://i.ytimg.com/vi/3Bk6VPWGi1o/default.jpg'
-//         }
-//       }
-//     }
-//   }
-// ];
 
 export default YouTubeContainer;

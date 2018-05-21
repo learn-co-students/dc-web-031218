7fa3cd0b7a266ab77d0fafc92e8826ec27838865
Rob Cobb <rwcobbjr@gmail.com>
2018-05-21 09:16:47 -0400

    set up pre lecture

---
+++ 50-example-app/youtubr/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">YOUTUBE GOES HERE</div>;
+  }
+}
+
+export default App;
